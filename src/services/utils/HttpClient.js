6fdffc4c7c7f08da4cfe6e65373c5f49376244e5
João Victor Pereira de Andrade <jv.andrade02@gmail.com>
2024-12-18 08:58:37 -0300

    feat: parse the response body when is an application json

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -8,7 +8,19 @@ class HttpClient {
 
     const response = await fetch(`${BASE_URL}${path}`);
 
-    return response.json();
+    const contentType = response.headers.get('Content-Type');
+
+    let body = null;
+
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) return body;
+
+    throw new Error(
+      body?.error || `${response.status} - ${response.statusText}`
+    );
   }
 }
 

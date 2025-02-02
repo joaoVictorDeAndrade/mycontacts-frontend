47a2f656ff9d5555792f4220477dc7f98db06dc8
João Victor Pereira de Andrade <jv.andrade02@gmail.com>
2025-02-02 12:12:45 -0300

    refactor: create useHome hook

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,4 @@
-import { useEffect, useMemo, useCallback, useState } from 'react';
 import { formatPhone } from '../../utils/formatPhone.js';
-import { fullNormalize } from '../../utils/fullNormalize.js';
 
 import { Link } from 'react-router-dom';
 
@@ -25,95 +23,27 @@ import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
 import magnifierQuestion from '../../assets/images/magnifier-question.svg';
-
-import ContactsService from '../../services/ContactsService.js';
-import { toast } from '../../utils/toast.js';
+import { useHome } from './useHome.js';
 
 export default function Home() {
-  const [contacts, setContacts] = useState([]);
-  const [orderBy, setOrderBy] = useState('asc');
-  const [searchTerm, setSearchTerm] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
-  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
-  const [contactToDelete, setContactToDelete] = useState(null);
-  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
-
-  const filteredContacts = useMemo(() => {
-    return contacts.filter((contact) => {
-      const name = fullNormalize(contact.name);
-      const search = fullNormalize(searchTerm);
-
-      return name.includes(search);
-    });
-  }, [contacts, searchTerm]);
-
-  const justifyContent = useMemo(() => {
-    if (hasError) return 'flex-end';
-
-    return contacts.length > 0 ? 'space-between' : 'center';
-  }, [hasError, contacts.length]);
-
-  const fetchContacts = useCallback(async () => {
-    try {
-      setIsLoading(true);
-
-      const contactsList = await ContactsService.fetchContacts(orderBy);
-      setHasError(false);
-      setContacts(contactsList);
-    } catch (error) {
-      setHasError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [orderBy]);
-
-  useEffect(() => {
-    fetchContacts();
-  }, [fetchContacts]);
-
-  function handleToggleOrderBy() {
-    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
-  }
-
-  function handleChangeSearchTerm(event) {
-    setSearchTerm(event.target.value);
-  }
-
-  function handleTryAgain() {
-    fetchContacts();
-  }
-
-  async function handleDeleteContact(contact) {
-    setIsDeleteModalVisible(true);
-    setContactToDelete(contact);
-  }
-
-  function closeDeleteModal() {
-    setIsDeleteModalVisible(false);
-    setContactToDelete(null);
-  }
-
-  async function deleteContact() {
-    try {
-      setIsLoadingDelete(true);
-      await ContactsService.deleteContact(contactToDelete.id);
-
-      toast({
-        type: 'success',
-        text: 'Contato deletado com sucesso!',
-      });
-    } catch (error) {
-      toast({
-        type: 'danger',
-        text: 'Ocorreu um erro ao deletar o contato!',
-      });
-    } finally {
-      closeDeleteModal();
-      setIsLoadingDelete(false);
-      await fetchContacts();
-    }
-  }
+  const {
+    isLoading,
+    isLoadingDelete,
+    isDeleteModalVisible,
+    contactToDelete,
+    contacts,
+    hasError,
+    filteredContacts,
+    searchTerm,
+    orderBy,
+    justifyContent,
+    deleteContact,
+    closeDeleteModal,
+    handleTryAgain,
+    handleToggleOrderBy,
+    handleDeleteContact,
+    handleChangeSearchTerm,
+  } = useHome();
 
   return (
     <Container>
---
+++ src/pages/Home/useHome.js
@@ -0,0 +1,111 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { fullNormalize } from '../../utils/fullNormalize.js';
+import { toast } from '../../utils/toast.js';
+
+import ContactsService from '../../services/ContactsService.js';
+
+export function useHome() {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [contactToDelete, setContactToDelete] = useState(null);
+  const [isLoadingDelete, setIsLoadingDelete] = useState(false);
+
+  const filteredContacts = useMemo(() => {
+    return contacts.filter((contact) => {
+      const name = fullNormalize(contact.name);
+      const search = fullNormalize(searchTerm);
+
+      return name.includes(search);
+    });
+  }, [contacts, searchTerm]);
+
+  const justifyContent = useMemo(() => {
+    if (hasError) return 'flex-end';
+
+    return contacts.length > 0 ? 'space-between' : 'center';
+  }, [hasError, contacts.length]);
+
+  const fetchContacts = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const contactsList = await ContactsService.fetchContacts(orderBy);
+      setHasError(false);
+      setContacts(contactsList);
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [orderBy]);
+
+  useEffect(() => {
+    fetchContacts();
+  }, [fetchContacts]);
+
+  function handleToggleOrderBy() {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  }
+
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function handleTryAgain() {
+    fetchContacts();
+  }
+
+  async function handleDeleteContact(contact) {
+    setIsDeleteModalVisible(true);
+    setContactToDelete(contact);
+  }
+
+  function closeDeleteModal() {
+    setIsDeleteModalVisible(false);
+    setContactToDelete(null);
+  }
+
+  async function deleteContact() {
+    try {
+      setIsLoadingDelete(true);
+      await ContactsService.deleteContact(contactToDelete.id);
+
+      toast({
+        type: 'success',
+        text: 'Contato deletado com sucesso!',
+      });
+    } catch (error) {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao deletar o contato!',
+      });
+    } finally {
+      closeDeleteModal();
+      setIsLoadingDelete(false);
+      await fetchContacts();
+    }
+  }
+
+  return {
+    isLoading,
+    isLoadingDelete,
+    isDeleteModalVisible,
+    contactToDelete,
+    contacts,
+    hasError,
+    filteredContacts,
+    searchTerm,
+    orderBy,
+    justifyContent,
+    deleteContact,
+    closeDeleteModal,
+    handleTryAgain,
+    handleToggleOrderBy,
+    handleDeleteContact,
+    handleChangeSearchTerm,
+  };
+}
